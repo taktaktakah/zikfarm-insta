509029b3fc6fcb5f67b45b8a4425927e4eae57c7
seung tak <h99488201@gmail.com>
2017-10-23 11:03:25 +0000

    171023

---
+++ db/migrate/20171023085341_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+
+      t.text :content
+      t.text :image
+      
+      
+      t.timestamps null: false
+    end
+  end
+end
